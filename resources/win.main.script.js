c0b4ed30f9b9bd1a57ade12450f8991eea2e7156
DAC098 <dac098Git@gmail.com>
2016-08-20 20:14:56 -0700

    update to match
    updated the html and script to match the new files

--- resources/win.main.script.js
+++ resources/win.main.script.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const {render} = require('react-dom');
 
-const App = require('./reactjs/App.js');
+const App = require('./reactjs/containers/App.js');
 
 render(React.createElement(App),document.getElementById("render"));
